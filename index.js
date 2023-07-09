23e9b3ef8ab95d366592e668bc59473dd33b8938
tran van phat <tranphat701@gmail.com>
2023-07-09 17:24:23 +0700

    middleware

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
+
+// Custom middleware
+const secret_key = "12345";
+const keySecretMiddleware = (req, res, next) => {
+  const { keySecret } = req.params;
+  if (keySecret === "12345") {
+    // Nếu keySecret khớp với secret key của bạn
+    next(); // Cho phép tiếp tục xử lý yêu cầu
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+};
 
 server.use(middlewares);
+server.use(`/url/${secret_key}`, keySecretMiddleware); // Áp dụng middleware cho đường dẫn "/url/key-secret"
 server.use(router);
 
 server.listen(port);
